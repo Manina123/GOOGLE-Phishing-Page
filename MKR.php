09c0cf38ec41b7d97bef6a4ae7319505740f2139
ManojKashyap <manoj.manina@gmail.com>
2020-04-03 20:03:00 +0530

    Add files via upload

---
+++ MKR.php
@@ -0,0 +1,7 @@
+<html>
+    <head><title>MKR Site</title></head>
+    <body>
+      <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</p>
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<a href="https://www.gmail.com/"><img style="display: block; margin-left: auto; margin-right: auto;" src="gmail.gif" alt="" width="auto" height="auto" /></a></p> </body>
+</html>
